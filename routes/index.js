33e2de80fa105152795351a3e07a96c499deb18f
sushmita_SushmitaRout <sushmita@leewayhertz.com>
2022-04-22 15:44:53 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
  * Created by AyushK on 18/09/20.
  */
 import * as ValidationManger from "../middleware/validation";
-import auth from "../middleware/auth"
 import TestModule from "../app/modules/testModule";
 import {apiEndpoints, stringConstants} from "../app/common/constants";
 import User from '../app/modules/user'
